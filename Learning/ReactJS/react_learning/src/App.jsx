88c2d941bdf1382137b3dd6286bfa678c35b989d
Abdullah Azhar <abdullahazhar202rr@gmail.com>
2025-03-28 16:16:11 +0500

    ReactJs learning and other files added

---
+++ Learning/ReactJS/react_learning/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function App() {
+  return (
+    <div className='w-full h-screen flex content-center'>
+      <div className='bg-yellow-300 w-32 h-32 m-auto'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ Learning/ReactJS/react_learning/src/btn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function btn() {
+  return (
+    <div className='text-black'>btn</div>
+  )
+}
+
+export default btn  
