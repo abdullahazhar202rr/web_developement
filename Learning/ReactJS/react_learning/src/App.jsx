687101c51fbc2d5b321e953ebc7f7a4d777a5a70
Abdullah Azhar <abdullahazhar202rr@gmail.com>
2025-03-28 17:35:27 +0500

    react files arranged and tailwindcss installed

--- Learning/ReactJS/react_learning/src/App.jsx
+++ Learning/ReactJS/react_learning/src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from 'react'
+import Btn from './btn.jsx'
 function App() {
   return (
-    <div className='w-full h-screen flex content-center'>
-      <div className='bg-yellow-300 w-32 h-32 m-auto'>
-        
+    <div className="w-full h-screen bg-red-400 flex content-center">
+      <div className='w-96 h-96 bg-zinc-600 m-auto flex items-center justify-center'>
+        <Btn/>
       </div>
     </div>
   )
--- Learning/ReactJS/react_learning/src/btn.jsx
+++ Learning/ReactJS/react_learning/src/btn.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
 function btn() {
   return (
-    <div className='text-black'>btn</div>
+    <button className='bg-amber-400 p-1 rounded-2xl'>
+        Click Me
+    </button>
   )
 }
 
-export default btn  
+export default btn
