df252dbe4875b3dcca273cba311cdc2ea0f1421a
Abdullah Azhar <abdullahazhar202rr@gmail.com>
2025-12-10 12:45:04 +0500

    Added details in page.js

--- Projects/Portfolio-2/portfolio/app/page.js
+++ Projects/Portfolio-2/portfolio/app/page.js
@@ -8,21 +8,44 @@ import Navbar from "@/components/UI/Navbar";
 
 export default function Page() {
   return (
-    <div className="relative w-full h-full overflow-hidden">
+    <>
+      {/* Schema Markup for Google (JSON-LD) */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            "name": "Abdullah Azhar",
+            "url": "https://abdullahazhar202rr.vercel.app/",
+            "image": "https://avatars.githubusercontent.com/u/180684670?v=4",
+            "sameAs": [
+              "https://www.linkedin.com/in/abdullahazhar202",
+              "https://github.com/abdullahazhar202rr"
+            ],
+            "jobTitle": "Aspiring AI Engineer | Programming Enthusiast | Problem Solver | Full Stack Developer",
+            "description": "Bachelor’s in Artificial Intelligence student at The University of Faisalabad. Skilled in Python for AI applications, C++, HTML, CSS, API integration, and web development.",
+            "alumniOf": "The University of Faisalabad"
+          }),
+        }}
+      />
 
-      <header>
-        <Navbar />
-      </header>
+      <div className="relative w-full h-full overflow-hidden">
 
-      <main>
-        <HeroSection />
-        <AboutSection />
-        <ProjectsSection />
-      </main>
+        <header>
+          <Navbar />
+        </header>
 
-      <footer>
-        <Footer />
-      </footer>
-    </div>
+        <main>
+          <HeroSection />
+          <AboutSection />
+          <ProjectsSection />
+        </main>
+
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </>
   );
 }
